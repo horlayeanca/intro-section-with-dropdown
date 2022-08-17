7b73fee057409173ad66e354e422e9d95499abba
Faruq Adelodun <horlayeanca@Faruqs-MacBook-Air.local>
2022-08-17 14:32:38 +0100

    still in progress

---
+++ index.js
@@ -0,0 +1,19 @@
+// const hamburger = document.querySelector('#hamburger')
+// const menu = document.querySelector('.hide')
+// const button = document.querySelector('.log')
+// const dropDown = document.querySelector('.drop')
+
+// document.querySelector('.dropdown_menu').style.display = 'none'
+
+// document.getElementById('.drop').addEventListener('click', (e) => {
+//     e.preventDefault() 
+//     document.querySelector('.dropdown_menu').style.display = 'block'
+// })
+
+// document.getElementById('hamburger').addEventListener('click', (e) => {
+//     e.preventDefault()
+//     document.querySelector('.hide').style.display = 'flex'
+//     document.querySelector('#hamburger').style.display = 'none'
+//     document.querySelector('.log').style.display = 'block'
+//     // document.querySelector('.menu_bar').style.display = 'flex'
+// })
